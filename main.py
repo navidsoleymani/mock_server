0971b11dab1a51568631040782f59108d6c627ec
navid <navidsoleymani@ymail.com>
2023-05-13 10:22:09 +0330

    add statics conf

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 from servers.datastoryboard.routers import datastoryboard as datastoryboard_router
 
 app = FastAPI()
 
+app.mount("/dsb-media", StaticFiles(directory="dsb-media"), name="statics")
 
 @app.get('/')
 async def hello():
