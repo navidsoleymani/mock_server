adefe50b5bbecb5fcf610333afdd32ea4a745fa2
navid <navidsoleymani@ymail.com>
2023-05-13 15:41:53 +0330

    cors

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 from servers.datastoryboard.routers import datastoryboard as datastoryboard_router
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/dsb-media", StaticFiles(directory="dsb-media"), name="statics")
 
 
